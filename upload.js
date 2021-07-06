a77cabc0e15e0e2da3a7500f579442327b5d5cdf
Alex Slomyanovskiy <erixvossa@yandex.ru>
2021-07-06 17:33:38 +0300

    init

---
+++ upload.js
@@ -0,0 +1,80 @@
+const input = document.getElementById('file-input');
+const video = document.getElementById('video');
+const videoSource = document.createElement('source');
+
+input.addEventListener('change', function() {
+    const files = this.files || [];
+
+    if (!files.length) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        videoSource.setAttribute('src', e.target.result);
+        video.appendChild(videoSource);
+        video.load();
+        //video.play();
+    };
+
+    reader.onprogress = function (e) {
+        console.log('progress: ', Math.round((e.loaded * 100) / e.total));
+    };
+
+    reader.readAsDataURL(files[0]);
+});
+
+var videoId = 'video';
+var scaleFactor = 0.25;
+var snapshots = [];
+
+const captureButton = document.querySelector('#cit');
+
+/**
+ * Captures a image frame from the provided video element.
+ *
+ * @param {Video} video HTML5 video element from where the image frame will be captured.
+ * @param {Number} scaleFactor Factor to scale the canvas element that will be return. This is an optional parameter.
+ *
+ * @return {Canvas}
+ */
+function capture(video, scaleFactor) {
+    if (scaleFactor == null) {
+        scaleFactor = 1;
+    }
+    var w = video.videoWidth * scaleFactor;
+    var h = video.videoHeight * scaleFactor;
+    var canvas = document.createElement('canvas');
+    canvas.width = w;
+    canvas.height = h;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, w, h);
+    return canvas;
+}
+
+/**
+ * Invokes the <code>capture</code> function and attaches the canvas element to the DOM.
+ */
+function shoot() {
+    var video = document.getElementById(videoId);
+    var output = document.getElementById('output');
+    var canvas = capture(video, scaleFactor);
+    canvas.onclick = function() {
+        window.open(this.toDataURL(image/jpg));
+    };
+    snapshots.unshift(canvas);
+    output.innerHTML = '';
+    for (var i = 0; i < 10; i++) {
+        output.appendChild(snapshots[i]);
+    }
+}
+
+captureButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    shoot();
+
+})
+
+// (function() {
+//     var captureit = document.getElementById('cit');
+//     captureit.click();
+// })();
